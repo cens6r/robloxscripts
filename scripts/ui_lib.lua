94586240bdaa1c745ae557bee8d1f8f8e8bf0cd5
censor <86632275+cens6r@users.noreply.github.com>
2022-09-02 22:01:27 +0900

    Create ui_lib.lua

---
+++ scripts/ui_lib.lua
@@ -0,0 +1,5 @@
+-- archive of https://raw.githubusercontent.com/shidemuri/scripts/main/ui_lib.lua
+
+--backup plan failed so here is the original script back
+--haha sang you came here just to read this youre so curious lmao 
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/shidemuri/scripts/main/newuilib.lua"))() 
