29c8ccfa7f5106e517d531e25354d1b6fd8ab5bc
censor <86632275+cens6r@users.noreply.github.com>
2022-09-27 19:27:55 +0900

    Update chat_bypasser.lua

--- scripts/universal/chat_bypasser.lua
+++ scripts/universal/chat_bypasser.lua
@@ -7,7 +7,7 @@
 -- // if instructions are followed you will have a 100% guarantee of no ban
 
 -- // Chat Bypasser
-loadstring(game:HttpGet('https://raw.githubusercontent.com/katrina367/ScriptBlox-Projects/main/Chat%20Bypasser'))()
+loadstring(game:HttpGet('https://github.com/cens6r/robloxscripts/raw/main/scripts/etc/chat_bypasser.lua'))() -- original: https://raw.githubusercontent.com/katrina367/ScriptBlox-Projects/main/Chat%20Bypasser
 
 -- // synapse (recommended) anti chat ban/logger
 loadstring(game:HttpGet('https://raw.githubusercontent.com/katrina367/ScriptBlox-Projects/main/Anti%20Chat%20Banner'))() 
