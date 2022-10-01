6f846a882a1d50ad17965aaf5edd356de125185f
censor <86632275+cens6r@users.noreply.github.com>
2022-10-01 22:51:13 +0900

    Create inf_money.lua

---
+++ scripts/games/festivaltycoon/inf_money.lua
@@ -0,0 +1,5 @@
+-- scriptblox thread: https://scriptblox.com/script/NEW-UGC-Festival-Tycoon-Inf-Money-6165
+
+-- scriptblox desc: Reupload.
+
+game.ReplicatedStorage.RemoteObjects.DanceGameCash:FireServer(9e9)
