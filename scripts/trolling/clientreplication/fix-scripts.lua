476fa828e3be7871ad6992910b7c521a2ab035ec
censor <86632275+cens6r@users.noreply.github.com>
2022-10-03 17:17:08 +0900

    Create fix-scripts.lua

---
+++ scripts/trolling/clientreplication/fix-scripts.lua
@@ -0,0 +1,18 @@
+--[[
+Deleted User — 12/31/2021
+
+No Problem Mad aLL bY Me #Shout out to @Arian For Writing The description.
+]]
+
+--[[SCRIPT:
+
+Join the server in social links section for the script.
+
+----------------------------------------------------
+To fix some scripts, put the line of code below at the start of your script:
+
+local script = Instance.new("Model", game:GetService("Players").LocalPlayer.Character)
+----------------------------------------------------
+
+
+(NOTE: This game was inspired by furrybuild(@letsbuild)'s game. This was originally his idea, however I decided to remake it.)]]
