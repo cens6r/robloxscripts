18b3760ec963d453221c9f8512d365a4030e4119
censor <86632275+cens6r@users.noreply.github.com>
2022-10-11 00:19:42 +0900

    Create cryptonic-hub.lua

---
+++ scripts/games/ragdoll-engine/cryptonic-hub.lua
@@ -0,0 +1,7 @@
+--[[
+CryptonicHub <3
+script made by martiin#0015, metaa#0001 and demvkrao#0069
+https://www.roblox.com/games/9848789324/Ragdoll-Engine
+--]]
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/martinelcrac/cryptonichub/main/Ragdollengine.lua'))()
