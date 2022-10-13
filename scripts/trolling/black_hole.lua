9059d2d96efe1e6b1cb339e6b6e74f5dec4820f7
censor <86632275+cens6r@users.noreply.github.com>
2022-10-13 23:42:53 +0900

    Create black_hole.lua

---
+++ scripts/trolling/black_hole.lua
@@ -0,0 +1,56 @@
+--discord.gg/ZKVkd8N3w9 FOR KEY
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/vilRBLX/Linorix/main/.github/workflows/linorix.lua'),true))()
+
+--//CMDS//--
+--net fix
+--sonic
+--gale
+--spider
+--head pet
+--ragicide
+--scp
+--among us
+--part mover
+--big head
+--book reader
+--hoverboard
+--gravity gun
+--slenderman
+--skateboard
+--pog dance
+--pumpkin titan
+--star glitcher
+--fallen anger
+--car
+--bag
+--fake lag
+--dog pet
+--showcase dummy
+--strong boi
+--cat pet
+--soldier ai
+--nullion
+--neko
+--hamster ball
+--combine soldier
+--attack cat
+--headfling
+--ball
+--graphics
+--pacifist
+--spiderman
+--sword glider
+--eye head
+--darth vader
+--guts
+--minigun
+--elio blasio
+--chara
+--killer
+--billy
+--popcat
+--creature
+--minecart
+--upside down
+--beacon
+--waiter
